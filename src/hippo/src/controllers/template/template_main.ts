bf9f06a625dd294647e3a4015fd00f7f67691568
MatyasHroch <matyas.hroch01@upol.cz>
2025-03-09 19:02:15 +0100

    Just a few changes for clarity of the experiment.

--- src/hippo/src/controllers/template/template_main.ts
+++ src/hippo/src/controllers/template/template_main.ts
@@ -5,25 +5,10 @@ import {processNodes} from "./template_getters";
 export async function renderTemplate(template:Element, context: Context, nodesToSLot: Array<Element> = null) {
     // const clonedTemplate = template.cloneNode(true) as Element; // first we will clone it so we will not change the original template
     const clonedTemplate = template
-
-    // for now, first get the nodes and attributes and then bind and model them
-    // TODO - optimize this -> bind the text nodes and attributes while getting them
-    // TODO bind and model all attributes
-    // TODO render all text nodes (vars or props) inside the process
+    // TODO - optimize this -> bind the text nodes and attributes while getting them - DONE
+    // TODO bind and model all attributes - DONE
+    // TODO render all text nodes (vars or props) inside the process - DONE
     const {textNodes, attributeNodes, childComponents} = await processNodes(clonedTemplate, context, nodesToSLot);
-
-    // console.log("from the process nodes we have:")
-    // console.log({textNodes})
-    // console.log({attributeNodes})
-    // console.log({childComponents})
-
-    // bindAttributes(context, attributeNodes.toBind);
-    // modelAttributes(context, attributeNodes.toModel);
-
-
-    // TODO render all classes that uses variables or properties (vars or props)
-    // const classNodes = getClassNodes(clonedTemplate);
-    // renderClasses(classNodes, variables)
 
     return {clonedTemplate, childComponents};
 }
